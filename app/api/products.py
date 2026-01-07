0295560dd88f018f5723e5f536a5e7f6bb5d0408
OneSummerDay <artem.kraievyi@gmail.com>
2026-01-07 21:20:37 +0200

    refactor: move product DB logic from routes to service

--- app/api/products.py
+++ app/api/products.py
@@ -5,6 +5,7 @@ from sqlalchemy import select
 from app.db.session import get_session
 from app.db.models import TrackedProduct
 from app.schemas.product import ProductCreate, ProductRead
+from app.services.product_service import ProductService
 
 router = APIRouter(prefix="/products", tags=["products"])
 
@@ -14,17 +15,14 @@ async def create_product(
     data: ProductCreate,
     session: AsyncSession = Depends(get_session)
 ):
-    product = TrackedProduct(name=data.name, url=data.url)
-
-    session.add(product)
-    await session.commit()
-    await session.refresh(product)
+    product = await ProductService.create_product(
+        data = data,
+        session = session)
     return product
     
 @router.get("/", response_model=list[ProductRead])
 async def read_products(
     session: AsyncSession = Depends(get_session)
 ):
-    result = await session.execute(select(TrackedProduct))
-    products = result.scalars().all()
+    products = await ProductService.read_products(session=session)
     return products
